511ba56fe0fe757c6c15f2ed5f0687508557ed96
ashwini lalithaHuchahanumegowda <ashwini.lh12@gmail>
2022-09-12 23:06:22 -0500

    about component test

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+afterEach(cleanup);
+const { asFragment } = render(<About />);
+describe('About component', () => {
+    //renders About test
+    // First Test
+  it('renders', () => {
+    asFragment;
+  });
+
+  // Second Test
+  it('matches snapshot DOM node structure', () => {
+    // render About
+    expect(asFragment()).toMatchSnapshot();
+    
+  });
+  })
